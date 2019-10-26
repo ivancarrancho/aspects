7119720deb6423a3ac7fb4c8b4577db0f290f11a
Ivan Ramirez <ivan.ramirez@senseta.com>
2019-10-26 16:11:02 -0500

    add new models

--- myvenv/app/admin.py
+++ myvenv/app/admin.py
@@ -1,4 +1,61 @@
 from django.contrib import admin
-from .models import Aspects
+from .models import Team
+from .models import Project
+from .models import UserHistory
+from django.contrib.auth.models import User
 
-admin.site.register(Aspects)
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name',
+    )
+
+    search_fields = ('name', 'id')
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'name',
+    )
+
+    search_fields = ('name', 'id')
+
+
+@admin.register(UserHistory)
+class UserHistoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'id',
+        'title',
+        'project',
+        'get_teams',
+    )
+
+    search_fields = ('title', 'id')
+
+    list_filter = ('project__name', 'project__team__name')
+
+    def get_teams(self, obj):
+        return obj.project.team
+
+    get_teams.short_description = 'Equipos'
+
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'get_groups', 'username', 'is_active')
+    list_filter = ('is_staff', 'is_superuser')
+
+    def get_groups(self, obj):
+        return list(obj.groups.values_list('name', flat=True))
+
+    get_groups.short_description = 'Grupos de usuario'
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
--- myvenv/app/models.py
+++ myvenv/app/models.py
@@ -1,15 +1,81 @@
 from django.db import models
-from django.utils import timezone
+from django.contrib.auth.models import User
 
 
-class Aspects(models.Model):
-    title = models.CharField(max_length=200)
-    history_type = models.TextField()
-    user = models.CharField(max_length=200)
-    description = models.TextField()
-    criteria_of_acceptance = models.TextField()
-    interface_requirements = models.TextField()
-    dependencies = models.TextField()
+class Team(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name='Nombre',
+        help_text='Nombre del equipo'
+    )
+
+    user = models.ManyToManyField(
+        User,
+        verbose_name='Usuario',
+        help_text='Usuarios que hacen parte del equipo'
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Equipo'
+        verbose_name_plural = 'Equipos'
+
+
+class Project(models.Model):
+    team = models.ForeignKey(
+        Team,
+        verbose_name='Equipo',
+        on_delete=models.PROTECT,
+        help_text='Equipo al que pertenece el proyecto'
+    )
+
+    name = models.CharField(
+        max_length=200,
+        verbose_name='nombre',
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Proyecto'
+        verbose_name_plural = 'Proyectos'
+
+
+class UserHistory(models.Model):
+    title = models.CharField(
+        max_length=200,
+        verbose_name='Título',
+    )
+    history_type = models.TextField(
+        verbose_name='Tipo de historia',
+    )
+
+    description = models.TextField(
+        verbose_name='descripción',
+    )
+    criteria_of_acceptance = models.TextField(
+        verbose_name='criterio de aceptación',
+    )
+    interface_requirements = models.TextField(
+        verbose_name='requerimientos de interfaz',
+    )
+    dependencies = models.TextField(
+        verbose_name='dependencias',
+    )
+
+    project = models.ForeignKey(
+        Project,
+        on_delete=models.PROTECT,
+        verbose_name='Proyecto',
+        help_text='Proyecto al que pertenece la historia de usuario'
+    )
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        verbose_name = 'Historia de usuario'
+        verbose_name_plural = 'Historia de usuarios'
